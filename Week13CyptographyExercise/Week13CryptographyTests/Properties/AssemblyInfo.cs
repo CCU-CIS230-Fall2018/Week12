21bbce862ab561b1b9e62ba8ae5944290a7cf2b2
coraw24 <cwiersema@students.ccu.edu>
2018-11-12 19:11:25 -0700

    Final

---
+++ Week13CyptographyExercise/Week13CryptographyTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Week13CryptographyTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Week13CryptographyTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5940b908-8279-4065-a28a-222bee9f386f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Week13CyptographyExercise/Week13CyptographyExercise/Hash.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Week13CyptographyExercise
+{
+    public class Hash
+    {
+        public static byte[] HashData(string text)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(text);
+            byte[] hashData = SHA256.Create().ComputeHash(data);
+
+            return hashData;
+        }
+    }
+}
