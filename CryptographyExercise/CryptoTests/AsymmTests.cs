8e504f8b791e766fadfa498bf54398268b9b9f9b
Keaton Helm <khelm@students.ccu.edu>
2018-11-15 21:17:21 -0700

    Here.

---
+++ CryptographyExercise/CryptoTests/AsymmTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CryptographyExercise;
+
+namespace CryptoTests
+{
+    [TestClass]
+    public class AsymmTests
+    {
+        [TestMethod]
+        public void AsymmetricEncryptTest()
+        {
+            string testMessage = "Too many secrets..";
+
+            string testMessageEncrypted = AsymmetricEncryptionator.EncryptTarget(testMessage);
+
+            //Prove message is secured but still the original.
+            Assert.AreEqual(testMessage, testMessageEncrypted);
+        }
+    }
+}
---
+++ CryptographyExercise/CryptographyExercise/AsymmetricEncryptionator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CryptographyExercise
+{
+    public class AsymmetricEncryptionator
+    {
+
+        public static string EncryptTarget(string target)
+        {
+            string encryptionString = target;
+            string publicKey;
+            string privateKey;
+            byte[] encryptedTarget;
+            string decryptedTarget;
+
+            // Innitiate keys.
+            using (var csp = new RSACryptoServiceProvider())
+            {
+                privateKey = csp.ToXmlString(true);
+                publicKey = csp.ToXmlString(false);
+            }
+
+            // Encryption target with public key.
+            using (var csp = new RSACryptoServiceProvider())
+            {
+                csp.FromXmlString(publicKey);
+
+                encryptedTarget = csp.Encrypt(Encoding.UTF8.GetBytes(encryptionString), true);
+            }
+
+            // Decrypts target using the private key.
+            using (var csp = new RSACryptoServiceProvider())
+            {
+                csp.FromXmlString(privateKey);
+
+                decryptedTarget = Encoding.UTF8.GetString(csp.Decrypt(encryptedTarget, true));
+            }
+
+            return decryptedTarget;
+        }
+    }
+}
